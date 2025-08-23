6347d840d51b76a61c478d3cccedfa6ef837823c
Mohammed Al-Mustafa <alraqym050@gmail.com>
2025-08-23 21:12:52 +0300

    add module translation and Docs

---
+++ jawaher_pos_customization/README.md
@@ -0,0 +1,28 @@
+# Jawaher POS Customization
+
+This module add custom features to the Point of Sale (POS) module in Odoo.
+
+## Developers
+
+- **[Mohammed Al-Mustafa Adel](https://t.me/M_IT_01)**
+
+## Features
+
+- Show Product Sale Price on POS product screen.
+- Show Product Barcode Text on POS product screen.
+
+## Enabling the Features
+
+Features can be enabled from the POS configuration settings:
+
+1. Activate single company before starting.
+2. Go to Point of Sale > Press on edit action on your sale point (3 dots on sale point card).
+3. Click on  "More settings: Configurations > Settings" link.
+4. Enable "Show Product Sale Price" and/or "Show Product Barcode Text" options (check the boxes).
+5. Save the changes.
+   After enabling the features, refresh the POS page to see the changes.
+
+## Module Information
+
+- module name should be `jawaher_pos_customization`
+- version: `18.0.0.0`
--- jawaher_pos_customization/__manifest__.py
+++ jawaher_pos_customization/__manifest__.py
@@ -4,12 +4,18 @@
     'category': 'Point of Sale',
     'summary': "Module contains customization for Point of Sale in Odoo",
     'description': """ 
-
-        Show Product Sale Price on POS in odoo,
-        Show Product Barcode Text on POS in odoo,
+        ## Features
+        - Show Product Sale Price on POS product screen.
+        - Show Product Barcode Text on POS product screen.
         
-        Those options can be enabled|disabled for each company in POS configuration.
-
+        ## Enabling the Features
+        Features can be enabled from the POS configuration settings:
+        1. Activate single company before starting.
+        2. Go to Point of Sale > Press on edit action on your sale point (3 dots on sale point card).
+        3. Click on  "More settings: Configurations > Settings" link.
+        4. Enable "Show Product Sale Price" and/or "Show Product Barcode Text" options (check the boxes).
+        5. Save the changes.
+        After enabling the features, refresh the POS page to see the changes.
     """,
     "author": "@JAWAHER/M.Al-Mustafa",
     "website": "http://jawaher.co/",
